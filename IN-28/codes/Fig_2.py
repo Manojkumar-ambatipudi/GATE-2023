9d48f7fd10eb34bb8c8988fd01a7130930fff055
manoj kumar <manoj-kumar@manoj-kumar.iith.ac.in>
2024-01-30 12:23:03 +0530

    first

---
+++ IN-28/codes/Fig_2.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+
+def u(n):
+    return np.heaviside(n,0)
+
+x = np.linspace(-8,8,10000)
+plt.plot(x, u(-x+5),'r')
+plt.plot(x, u(x+3),'b')
+plt.title('plots of functions h(n) and f(n)')
+plt.grid(1)
+plt.xlabel('n')
+plt.ylabel('f(n), h(n)')
+plt.savefig('fig_2.png')
+
