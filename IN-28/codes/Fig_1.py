730d5db76d1dc303007057f6f5b95397606400bb
manoj kumar <manoj-kumar@manoj-kumar.iith.ac.in>
2024-01-31 14:25:24 +0530

    first

--- IN-28/codes/Fig_1.py
+++ IN-28/codes/Fig_1.py
@@ -2,12 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def u(n):
-    return np.heaviside(n, 0)
+    return np.heaviside(n, 1)
 
-x = np.linspace(-8, 8, 10000)
-plt.plot(x, u(-x+5) - u(x+3))
+x = np.arange(-8, 9)
+plt.stem(x, u(-x+5)-u(x+3))
 plt.title("Plot of x(n) function")
 plt.grid(1)
 plt.xlabel('n')
 plt.ylabel('x(n)')
 plt.savefig('fig_1.png')
+
+plt.clf()
+x = np.arange(-8, 9)
+plt.stem(x, u(x+3),'b')
+plt.title('plot of function u(n+3)')
+plt.grid(1)
+plt.xlabel('n')
+plt.ylabel('u(n+3)')
+plt.savefig('fig_2.png')
+
+plt.clf()
+x = np.arange(-8, 9)
+plt.stem(x, u(-x+5),'g')
+plt.title('plot of function u(-n+5)')
+plt.grid(1)
+plt.xlabel('n')
+plt.ylabel('u(-n+5)')
+plt.savefig('fig_3.png')
+
